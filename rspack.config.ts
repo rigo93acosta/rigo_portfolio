12ee6cd86795142f7a32cc3265023dab8f45d758
rigo93acosta <rigo93acosta@gmail.com>
2026-02-12 17:41:27 -0300

    Sitio personal

--- rspack.config.ts
+++ rspack.config.ts
@@ -1,66 +1,21 @@
 import { defineConfig } from '@rspack/cli';
-import { rspack, type SwcLoaderOptions } from '@rspack/core';
-import { ReactRefreshRspackPlugin } from '@rspack/plugin-react-refresh';
+import { rspack } from '@rspack/core';
+import RefreshPlugin from '@rspack/plugin-react-refresh';
+// 1. Importamos Zephyr
+import { withZephyr } from 'zephyr-webpack-plugin';
 
-const isDev = process.env.NODE_ENV === 'development';
-
-// Target browsers, see: https://github.com/browserslist/browserslist
-const targets = ['last 2 versions', '> 0.2%', 'not dead', 'Firefox ESR'];
-
-export default defineConfig({
+const config = defineConfig({
+  context: __dirname,
   entry: {
     main: './src/main.tsx',
-  },
-  resolve: {
-    extensions: ['...', '.ts', '.tsx', '.jsx'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.svg$/,
-        type: 'asset',
-      },
-      {
-        test: /\.(jsx?|tsx?)$/,
-        use: [
-          {
-            loader: 'builtin:swc-loader',
-            options: {
-              jsc: {
-                parser: {
-                  syntax: 'typescript',
-                  tsx: true,
-                },
-                transform: {
-                  react: {
-                    runtime: 'automatic',
-                    development: isDev,
-                    refresh: isDev,
-                  },
-                },
-              },
-              env: { targets },
-            } satisfies SwcLoaderOptions,
-          },
-        ],
-      },
-    ],
   },
   plugins: [
     new rspack.HtmlRspackPlugin({
       template: './index.html',
     }),
-    isDev ? new ReactRefreshRspackPlugin() : null,
+    new RefreshPlugin(),
   ],
-  optimization: {
-    minimizer: [
-      new rspack.SwcJsMinimizerRspackPlugin(),
-      new rspack.LightningCssMinimizerRspackPlugin({
-        minimizerOptions: { targets },
-      }),
-    ],
-  },
-  experiments: {
-    css: true,
-  },
 });
+
+// 2. Envolvemos la configuración con withZephyr()
+export default withZephyr()(config);
